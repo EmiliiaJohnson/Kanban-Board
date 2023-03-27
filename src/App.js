749042c5ff80b564095d57d0b57aa34c6e7d3a70
Emiliia Johnson <117929541+EmiliiaJohnson@users.noreply.github.com>
2023-03-27 12:16:00 +0400

    Switch from BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './components/header/header';
 import Main from './components/main/main';
 import Footer from './components/footer/footer';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 function App() {
